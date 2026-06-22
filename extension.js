a266a2ab28770545a981563b6059ed051063f745
Jacq van Jaarsveld <jacqvanjaarsveld@gmail.com>
2026-06-22 22:48:19 +0200

    Wait for startup-complete before positioning corner button

--- extension.js
+++ extension.js
@@ -30,7 +30,14 @@ export default class ShowSystemMenuHotCorner extends Extension {
             this
         );
 
-        this._updatePosition();
+        if (Main.layoutManager._startingUp) {
+            Main.layoutManager.connectObject(
+                'startup-complete', () => this._updatePosition(),
+                this
+            );
+        } else {
+            this._updatePosition();
+        }
     }
 
     _trigger() {
